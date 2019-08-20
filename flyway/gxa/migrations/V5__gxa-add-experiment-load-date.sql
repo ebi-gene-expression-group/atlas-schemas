67896bf838f13729d35619a6582be00b484d3c9a
amunozpomer <amunoz@ebi.ac.uk>
2019-08-20 17:26:10 +0100

    Add default value to load_date

--- flyway/gxa/migrations/V5__gxa-add-experiment-load-date.sql
+++ flyway/gxa/migrations/V5__gxa-add-experiment-load-date.sql
@@ -1,5 +1,5 @@
 ALTER TABLE experiment
-ADD COLUMN load_date TIMESTAMP;
+ADD COLUMN load_date TIMESTAMP DEFAULT NOW();
 
 UPDATE experiment
 SET load_date = last_update;
--- flyway/scxa/migrations/V5__scxa-add-experiment-load-date.sql
+++ flyway/scxa/migrations/V5__scxa-add-experiment-load-date.sql
@@ -1,5 +1,5 @@
 ALTER TABLE scxa_experiment
-ADD COLUMN load_date TIMESTAMP;
+ADD COLUMN load_date TIMESTAMP DEFAULT NOW();
 
 UPDATE scxa_experiment
 SET load_date = last_update;
