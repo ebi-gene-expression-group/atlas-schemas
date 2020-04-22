fe2c25ca88ffae8207b7e3a6c2080e1e03af14f0
Jonathan Manning <jmanning@ebi.ac.uk>
2020-04-22 11:20:21 +0100

    Propose normalised structure for new cell group markers

--- db/gxasc-schema.sql
+++ db/gxasc-schema.sql
@@ -42,6 +42,34 @@ CREATE TABLE scxa_cell_clusters
   PRIMARY KEY (experiment_accession, k, cell_id)
 );
 
+CREATE TABLE scxa_cell_group
+(
+  id SERIAL not null PRIMARY KEY,
+  experiment_accession VARCHAR(255) NOT NULL,
+  variable VARCHAR(255) NOT NULL,
+  value VARCHAR(255) NOT NULL,
+  CONSTRAINT scxa_cell_group_experiment_accession_variable_value_pk
+  UNIQUE (experiment_accession, variable, value)  
+);
+
+CREATE TABLE scxa_cell_group_membership
+(
+  experiment_accession VARCHAR(255) NOT NULL,
+  cell_id VARCHAR(255) NOT NULL,
+  cell_group_id integer references scxa_cell_group(id)
+  CONSTRAINT scxa_cell_clusters_experiment_accession_cell_id_cell_group_id_pk
+  PRIMARY KEY (experiment_accession, cell_id, cell_group_id)
+);
+
+CREATE TABLE scxa_cell_group_marker_genes
+(
+  gene_id VARCHAR(255) NOT NULL,
+  cell_group_id integer references scxa_cell_group(id)
+  marker_probability DOUBLE PRECISION,
+  CONSTRAINT scxa_cell_group_marker_genes_pkey
+  PRIMARY KEY (gene_id, cell_group_id)
+);
+
 -- This table replaces the materialised view used in the Postgres DB
 CREATE TABLE scxa_marker_gene_stats
 (
