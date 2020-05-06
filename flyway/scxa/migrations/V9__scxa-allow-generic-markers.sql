c834290948e4179ba03e577de5f246dc1569bfe3
Jonathan Manning <jmanning@ebi.ac.uk>
2020-05-06 14:58:49 +0100

    Move changes to migrations and propose summary stats table

---
+++ flyway/scxa/migrations/V9__scxa-allow-generic-markers.sql
@@ -0,0 +1,42 @@
+
+CREATE TABLE scxa_cell_group
+(
+  id SERIAL not null PRIMARY KEY,
+  experiment_accession VARCHAR(255) NOT NULL,
+  variable VARCHAR(255) NOT NULL,
+  value VARCHAR(255) NOT NULL,
+  UNIQUE (experiment_accession, variable, value)  
+);
+
+CREATE TABLE scxa_cell_group_membership
+(
+  experiment_accession VARCHAR(255) NOT NULL,
+  cell_id VARCHAR(255) NOT NULL,
+  cell_group_id integer references scxa_cell_group(id)
+  CONSTRAINT scxa_cell_clusters_experiment_accession_cell_id_cell_group_id_pk
+  PRIMARY KEY (experiment_accession, cell_id, cell_group_id)
+);
+
+CREATE TABLE scxa_cell_group_marker_genes
+(
+  id SERIAL not null PRIMARY KEY,
+  gene_id VARCHAR(255) NOT NULL,
+  cell_group_id integer references scxa_cell_group(id)
+  marker_probability DOUBLE PRECISION,
+  CONSTRAINT scxa_cell_group_marker_genes_pkey,
+  PRIMARY KEY (gene_id, cell_group_id),
+  UNIQUE (gene_id, cell_group_id)  
+);
+
+-- Create a table from data prepared during bundling, replacing the previously used materialised view
+
+CREATE TABLE scxa_cell_group_marker_gene_stats
+(
+  gene_id VARCHAR(255) NOT NULL,
+  cell_group_id integer references scxa_cell_group(id),
+  marker_id integer references scxa_cell_group_marker_genes(id),
+  mean_expression DOUBLE PRECISION,
+  median_expression DOUBLE PRECISION
+  UNIQUE (gene_id, cell_group_id, marker_id) 
+); 
+    
