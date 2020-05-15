c5bc88eade08f71d93d0890081dda761b6ee56d2
Jonathan Manning <jmanning@ebi.ac.uk>
2020-05-15 15:43:02 +0100

    Cascade delete from foreign keys

--- flyway/scxa/migrations/V9__scxa-allow-generic-markers.sql
+++ flyway/scxa/migrations/V9__scxa-allow-generic-markers.sql
@@ -12,7 +12,7 @@ CREATE TABLE scxa_cell_group_membership
 (
   experiment_accession VARCHAR(255) NOT NULL,
   cell_id VARCHAR(255) NOT NULL,
-  cell_group_id integer references scxa_cell_group(id),
+  cell_group_id integer references scxa_cell_group(id) ON DELETE CASCADE,
   CONSTRAINT scxa_cell_groups_experiment_accession_cell_id_cell_group_id_pk
     PRIMARY KEY (experiment_accession, cell_id, cell_group_id)
 );
@@ -21,7 +21,7 @@ CREATE TABLE scxa_cell_group_marker_genes
 (
   id SERIAL not null PRIMARY KEY,
   gene_id VARCHAR(255) NOT NULL,
-  cell_group_id integer references scxa_cell_group(id),
+  cell_group_id integer references scxa_cell_group(id) ON DELETE CASCADE,
   marker_probability DOUBLE PRECISION,
   UNIQUE (gene_id, cell_group_id)  
 );
@@ -31,8 +31,8 @@ CREATE TABLE scxa_cell_group_marker_genes
 CREATE TABLE scxa_cell_group_marker_gene_stats
 (
   gene_id VARCHAR(255) NOT NULL,
-  cell_group_id integer references scxa_cell_group(id),
-  marker_id integer references scxa_cell_group_marker_genes(id),
+  cell_group_id integer references scxa_cell_group(id) ON DELETE CASCADE,
+  marker_id integer references scxa_cell_group_marker_genes(id) ON DELETE CASCADE,
   expression_type smallint NOT NULL,
   mean_expression DOUBLE PRECISION NOT NULL,
   median_expression DOUBLE PRECISION NOT NULL,
