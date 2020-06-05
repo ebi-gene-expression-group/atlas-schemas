8048434c38ca1c440953c836011cc36b9d45e930
Jonathan Manning <jmanning@ebi.ac.uk>
2020-06-05 10:06:35 +0100

    Add generic coordinates table

---
+++ flyway/scxa/migrations/V11_scxa-add-generic_coordinates.sql
@@ -0,0 +1,11 @@
+CREATE TABLE scxa_coords
+(
+    experiment_accession VARCHAR(255) NOT NULL,
+    method VARCHAR(255) NOT NULL,
+    cell_id VARCHAR(255) NOT NULL,
+    x DOUBLE PRECISION,
+    y DOUBLE PRECISION,
+    parameterisation VARCHAR(255) NOT NULL,
+    CONSTRAINT scxa_coords_experiment_accession_method_cell_id_parameterisation_pk
+        PRIMARY KEY (experiment_accession, method, cell_id, parameterisation)
+);
