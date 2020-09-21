7708eb5ca9776e03419a0b16115f34238a936cf6
Suhaib Mohammed <suhaib@ebi.ac.uk>
2020-09-21 17:09:34 +0100

    removed copying folders to container as it mounted from locally repo folders and removed atlas-schemas folder which didnt exist

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,8 +9,12 @@ USER=$(echo $dbConnection | sed s+postgresql://++ | sed 's+:.*++')
 PASS=$(echo $dbConnection | sed s+postgresql://++ | sed 's+.*:\(.*\)\@.*+\1+')
 HOST=$(echo $dbConnection | sed 's+.*\@\(.*\)/.*+\1+')
 DB=$(echo $dbConnection | sed 's+.*/\(.*\)+\1+')
-flyway migrate -url=jdbc:postgresql://${HOST}:5432/${DB} -user=${USER} -password=${PASS} -locations=filesystem:$( pwd )/atlas-schemas/flyway/scxa
 
+#SCXA
+flyway migrate -url=jdbc:postgresql://${HOST}:5432/${DB} -user=${USER} -password=${PASS} -locations=filesystem:$( pwd )/usr/local/flyway/scxa
+
+# GXA
+flyway migrate -url=jdbc:postgresql://${HOST}:5432/${DB} -user=${USER} -password=${PASS} -locations=filesystem:$( pwd )/usr/local/flyway/gxa
 
 if [ "$#" -eq 0 ]; then
 	bats --tap "$(dirname "${BASH_SOURCE[0]}")"
