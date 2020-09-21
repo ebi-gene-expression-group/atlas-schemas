9722fe1c900ecbe2f1929ddcaea4eda96afab972
Suhaib Mohammed <suhaib@ebi.ac.uk>
2020-09-21 13:12:06 +0100

    added containerisation of atlas schema and run tests scripts

---
+++ tests/run_tests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+scriptDir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export testsDir=$scriptDir
+export PATH=$scriptDir/../flyway:$scriptDir/../tests:$PATH
+
+# For analytics loading testing
+USER=$(echo $dbConnection | sed s+postgresql://++ | sed 's+:.*++')
+PASS=$(echo $dbConnection | sed s+postgresql://++ | sed 's+.*:\(.*\)\@.*+\1+')
+HOST=$(echo $dbConnection | sed 's+.*\@\(.*\)/.*+\1+')
+DB=$(echo $dbConnection | sed 's+.*/\(.*\)+\1+')
+flyway migrate -url=jdbc:postgresql://${HOST}:5432/${DB} -user=${USER} -password=${PASS} -locations=filesystem:$( pwd )/atlas-schemas/flyway/scxa
+
+
+if [ "$#" -eq 0 ]; then
+	bats --tap "$(dirname "${BASH_SOURCE[0]}")"
+else
+	bats "$@"
+fi
