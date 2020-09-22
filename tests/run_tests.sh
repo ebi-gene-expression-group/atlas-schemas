c912bbb1f6d0aa1599b9fee91a9924e21b94ed6c
Suhaib Mohammed <suhaib@ebi.ac.uk>
2020-09-22 10:29:01 +0100

    added optional schema to flyway

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -10,11 +10,11 @@ PASS=$(echo $dbConnection | sed s+postgresql://++ | sed 's+.*:\(.*\)\@.*+\1+')
 HOST=$(echo $dbConnection | sed 's+.*\@\(.*\)/.*+\1+')
 DB=$(echo $dbConnection | sed 's+.*/\(.*\)+\1+')
 
-#SCXA
-flyway migrate -url=jdbc:postgresql://${HOST}:5432/${DB} -user=${USER} -password=${PASS} -locations=filesystem:$( pwd )/usr/local/flyway/scxa
+# SCXA
+flyway migrate -schemas='scxa' -url=jdbc:postgresql://${HOST}:5432/${DB} -user=${USER} -password=${PASS} -locations=filesystem:$( pwd )/usr/local/flyway/scxa
 
 # GXA
-flyway migrate -url=jdbc:postgresql://${HOST}:5432/${DB} -user=${USER} -password=${PASS} -locations=filesystem:$( pwd )/usr/local/flyway/gxa
+flyway migrate -schemas='gxa' -url=jdbc:postgresql://${HOST}:5432/${DB} -user=${USER} -password=${PASS} -locations=filesystem:$( pwd )/usr/local/flyway/gxa
 
 if [ "$#" -eq 0 ]; then
 	bats --tap "$(dirname "${BASH_SOURCE[0]}")"
