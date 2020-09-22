cb7ebe442c09cce4ea6a974ba482e3a11d8aa86e
Suhaib Mohammed <suhaib@ebi.ac.uk>
2020-09-22 10:58:21 +0100

    same credentials for both with different schema

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,21 +7,22 @@ export PATH=$scriptDir/../flyway:$scriptDir/../tests:$PATH
 
 make_flyway_credentials(){
 	dbConnection=$1
+	schema=$2
 
 	USER=$(echo $dbConnection | sed s+postgresql://++ | sed 's+:.*++')
 	PASS=$(echo $dbConnection | sed s+postgresql://++ | sed 's+.*:\(.*\)\@.*+\1+')
 	HOST=$(echo $dbConnection | sed 's+.*\@\(.*\)/.*+\1+')
 	DB=$(echo $dbConnection | sed 's+.*/\(.*\)+\1+')
 
-	flyway migrate -schemas=$USER -url=jdbc:postgresql://${HOST}:5432/${DB} -user=${USER} -password=${PASS} -locations=filesystem:$( pwd )/usr/local/flyway/${USER}
+	flyway migrate -schemas=$schema -url=jdbc:postgresql://${HOST}:5432/${DB} -user=${USER} -password=${PASS} -locations=filesystem:$( pwd )/usr/local/flyway/${USER}
 
 }
 
 # SCXA
-make_flyway_credentials 'postgresql://scxa:postgresPass@postgres/scxa-test'
+make_flyway_credentials 'postgresql://scxa:postgresPass@postgres/scxa-test' 'scxa'
 
 # GXA
-make_flyway_credentials 'postgresql://gxa:postgresPass@postgres/gxa-test'
+make_flyway_credentials 'postgresql://scxa:postgresPass@postgres/scxa-test' 'gxa'
 
 if [ "$#" -eq 0 ]; then
 	bats --tap "$(dirname "${BASH_SOURCE[0]}")"
