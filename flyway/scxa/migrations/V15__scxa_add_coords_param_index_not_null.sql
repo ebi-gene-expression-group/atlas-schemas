278eaa03264833ff12957548278f1f691b829e62
Jonathan Manning <jmanning@ebi.ac.uk>
2021-02-23 15:57:14 +0000

    correct json index creatiion

--- flyway/scxa/migrations/V15__scxa_add_coords_param_index_not_null.sql
+++ flyway/scxa/migrations/V15__scxa_add_coords_param_index_not_null.sql
@@ -1,3 +1,3 @@
 ALTER TABLE scxa_coords ALTER COLUMN json_parameterisation SET NOT NULL;
 
-CREATE INDEX scxa_coords_json_parameterisation ON scxa_coords(json_parameterisation) using gin(json_parameterisation);
+CREATE INDEX scxa_coords_json_parameterisation ON scxa_coords using gin(json_parameterisation);
