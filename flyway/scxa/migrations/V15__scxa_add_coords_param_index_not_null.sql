15d6d8f71c2c56f322a3dd4e4990452eea63c40b
Jonathan Manning <jmanning@ebi.ac.uk>
2021-02-23 16:30:10 +0000

    Also drop old parameterisation column

--- flyway/scxa/migrations/V15__scxa_add_coords_param_index_not_null.sql
+++ flyway/scxa/migrations/V15__scxa_add_coords_param_index_not_null.sql
@@ -1,3 +1,4 @@
 ALTER TABLE scxa_coords ALTER COLUMN json_parameterisation SET NOT NULL;
+ALTER TABLE scxa_coords DROP COLUMN parameterisation;
 
 CREATE INDEX scxa_coords_json_parameterisation ON scxa_coords using gin(json_parameterisation);
