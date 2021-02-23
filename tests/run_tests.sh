3698d557df0b8683a12c9e34db49f0f035aa28fa
Jonathan Manning <jmanning@ebi.ac.uk>
2021-02-23 16:19:13 +0000

    Remove test statement

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -16,7 +16,6 @@ make_flyway_credentials(){
 	DB=$(echo $dbConnection | sed 's+.*/\(.*\)+\1+')
 
 	flyway migrate -schemas=$schema -url=jdbc:postgresql://${HOST}:5432/${DB} -user=${USER} -password=${PASS} -locations=filesystem:$( pwd )/usr/local/flyway/${schema}
-    echo "Flyway status: $?"
 }
 
 # SCXA
