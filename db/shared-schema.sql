d7705bb8e801d0f9b4fbe23b4d1a2750590fd642
Haider Iqbal <haideri@ebi.ac.uk>
2022-03-04 16:49:27 +0000

    Add table for exp_design

--- db/shared-schema.sql
+++ db/shared-schema.sql
@@ -35,3 +35,31 @@ CREATE TABLE designelement_mapping
     REFERENCES arraydesign
     ON DELETE CASCADE
 );
+
+CREATE TABLE exp_design
+(
+    experiment_accession VARCHAR(255)
+        CONSTRAINT exp_design_k
+            REFERENCES experiment (accession)
+            ON DELETE CASCADE,
+    run VARCHAR(50),
+    sample_type char(50)
+        CONSTRAINT exp_desing_sample_type_check
+            check (sample_type = ANY (ARRAY['characteristic'::bpchar, 'factor'::bpchar])),
+    sample VARCHAR(255),
+    sample_value VARCHAR(255),
+    ontology_url VARCHAR(255),
+    CONSTRAINT exp_design_pk
+        UNIQUE (experiment_accession, sample_type, sample_value)
+);
+
+CREATE TABLE exp_design_column_sequence
+(
+    experiment_accession VARCHAR(255) NOT NULL
+        CONSTRAINT column_sequence_pk
+            UNIQUE
+        CONSTRAINT exp_design_column_sequence_fk
+            REFERENCES experiment (accession)
+            ON DELETE CASCADE,
+    columns_sequence text[]
+);
