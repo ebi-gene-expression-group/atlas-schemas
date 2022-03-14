e60aa36da9d4053f6a47658c7e2f8a8ce8be5cf4
Jonathan Manning <jmanning@ebi.ac.uk>
2022-03-14 18:52:50 +0000

    Attempt to copy data and initialise column index

--- flyway/scxa/migrations/V18__anndata_import_changes.sql
+++ flyway/scxa/migrations/V18__anndata_import_changes.sql
@@ -12,16 +12,38 @@ CREATE TABLE scxa_dimension_reduction
 CREATE INDEX scxa_dimension_reduction_experiment_accession_method_param_priority ON scxa_dimension_reduction(experiment_accession, method, parameterisation, priority);
 CREATE INDEX scxa_dimension_reduction_id ON scxa_dimension_reduction(id);
 
+-- Populate with data from the old table
+
+INSERT INTO scxa_dimension_redution (experiment_accession, method, parameterisation, priority) 
+  SELECT DISTINCT experiment_accession, method, parameterisation, priority FROM scxa_coords;  
+
+-- Add new column
+
+ALTER TABLE scxa_coords
+  ADD COLUMN dimension_reduction_id SMALLINT REFERENCES scxa_dimension_reduction(id) ON DELETE CASCADE,
+
+-- Add the index for the new foreign key on the coords table
+CREATE_INDEX scxa_coords_dimension_reduction_id ON scxa_coords(dimension_reduction_id);
+
+-- Add the correct references to the new table
+
+INSERT INTO scxa_coords (dimension_reduction_id)
+  SELECT id
+  FROM scxa_coords LEFT JOIN scxa_dimension_reduction AS sdr ON 
+    scxa_coords.experiment_accession = sdr.experiment_accession AND
+    scxa_coords.method = sdr.method AND
+    scxa_coords.parameterisation = sdr.parameterisation
+
 -- Remove those columns from scxa_coords
 
 DROP INDEX scxa_coords_experiment_accession_method_param;
 
 ALTER TABLE scxa_coords
-  ADD COLUMN dimension_reduction_id SMALLINT REFERENCES scxa_dimension_reduction(id) ON DELETE CASCADE,
   DROP COLUMN experiment_accession,
   DROP COLUMN method,
   DROP COLUMN parameterisation;
 
--- Add the index for the new foreign key on the coords table
-CREATE_INDEX scxa_coords_dimension_reduction_id ON scxa_coords(dimension_reduction_id);
+
+
+
 
