c17249d3c2e41be7ce511ce749957d7642052c0e
Jonathan Manning <jmanning@ebi.ac.uk>
2022-03-14 19:01:22 +0000

    Fix syntax

--- flyway/scxa/migrations/V18__anndata_import_changes.sql
+++ flyway/scxa/migrations/V18__anndata_import_changes.sql
@@ -32,7 +32,7 @@ INSERT INTO scxa_coords (dimension_reduction_id)
   FROM scxa_coords LEFT JOIN scxa_dimension_reduction AS sdr ON 
     scxa_coords.experiment_accession = sdr.experiment_accession AND
     scxa_coords.method = sdr.method AND
-    scxa_coords.parameterisation = sdr.parameterisation
+    scxa_coords.parameterisation = sdr.parameterisation;
 
 -- Remove those columns from scxa_coords
 
