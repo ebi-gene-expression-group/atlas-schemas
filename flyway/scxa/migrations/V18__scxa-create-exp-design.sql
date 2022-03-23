8524ea07382a4af159a2b79aaec4054c511c45dd
Haider Iqbal <haideri@ebi.ac.uk>
2022-03-23 13:06:54 +0000

    Refactor schema of exp design table

--- flyway/scxa/migrations/V18__scxa-create-exp-design.sql
+++ flyway/scxa/migrations/V18__scxa-create-exp-design.sql
@@ -1,32 +1,27 @@
-CREATE TABLE exp_design
+create table exp_design
 (
-    accession VARCHAR(255)
-        CONSTRAINT exp_design_k
-            REFERENCES experiment (accession)
-            ON DELETE CASCADE,
-    marray VARCHAR(255),
-    sample VARCHAR(255),
-    annot_type char(50)
-        CONSTRAINT exp_design_annot_type_check
-            CHECK (annot_type = ANY (ARRAY['characteristic'::bpchar, 'factor'::bpchar])),
-    annot VARCHAR(255),
-    annot_value VARCHAR(255),
-    annot_ont_URI VARCHAR(255),
-    CONSTRAINT exp_design_pk
-        UNIQUE (accession, sample, annot_type)
+    sample varchar(255) not null,
+    annot_value varchar(255) not null,
+    annot_ont_URI varchar(255),
+    column_definition integer not null
+        constraint exp_design_exp_design_column_sequence_id_fk
+            references exp_design_column_sequence
+            on delete cascade,
+    id serial not null
+        constraint exp_design_pk
+            primary key
 );
 
-CREATE TABLE exp_design_column_sequence
+create table exp_design_column
 (
-    experiment_accession VARCHAR(255) NOT NULL
-        CONSTRAINT column_sequence_pk
-            UNIQUE
-        CONSTRAINT exp_design_column_sequence_fk
-            REFERENCES experiment (accession)
-            ON DELETE CASCADE,
-    column_name VARCHAR(255),
-    column_type CHAR(50)
-        CONSTRAINT column_type_check
-            CHECK (column_type = ANY (ARRAY['characteristic'::bpchar, 'factor'::bpchar])),
-    column_order INTEGER
+    experiment_accession varchar(255) not null
+        constraint exp_design_column_sequence_experiment_accession_fk
+            references experiment (accession)
+            on delete cascade,
+    column_name varchar(255) not null,
+    column_type boolean not null,
+    column_order integer not null,
+    id serial not null
+        constraint exp_design_column_sequence_pk
+            primary key
 );
