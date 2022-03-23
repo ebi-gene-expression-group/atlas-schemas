3c12c958e5e2620a734f58d87e5c123d85ba967a
Haider Iqbal <haideri@ebi.ac.uk>
2022-03-23 16:50:10 +0000

    Refactor GXA migration Refactor SCXA migration

--- flyway/gxa/migrations/V8__gxa-create-exp-design.sql
+++ flyway/gxa/migrations/V8__gxa-create-exp-design.sql
@@ -1,32 +1,21 @@
-CREATE TABLE exp_design
+CREATE TABLE exp_design_column
 (
-    accession VARCHAR(255)
-        CONSTRAINT exp_design_k
-            REFERENCES experiment (accession)
-            ON DELETE CASCADE,
-    marray VARCHAR(255),
-    sample VARCHAR(255),
-    annot_type char(50)
-        CONSTRAINT exp_design_annot_type_check
-            CHECK (annot_type = ANY (ARRAY['characteristic'::bpchar, 'factor'::bpchar])),
-    annot VARCHAR(255),
-    annot_value VARCHAR(255),
-    annot_ont_URI VARCHAR(255),
-    CONSTRAINT exp_design_pk
-        UNIQUE (accession, sample, annot_type)
+    experiment_accession VARCHAR(255) NOT NULL,
+    column_name VARCHAR(255) NOT NULL,
+    is_factor BOOLEAN NOT NULL,
+    column_order INTEGER NOT NULL,
+    id serial NOT NULL
+        CONSTRAINT exp_design_column_sequence_pk
+            PRIMARY KEY,
+    UNIQUE (experiment_accession, column_name, is_factor)
 );
 
-CREATE TABLE exp_design_column_sequence
+CREATE TABLE exp_design
 (
-    experiment_accession VARCHAR(255) NOT NULL
-        CONSTRAINT column_sequence_pk
-            UNIQUE
-        CONSTRAINT exp_design_column_sequence_fk
-            REFERENCES experiment (accession)
-            ON DELETE CASCADE,
-    column_name VARCHAR(255),
-    column_type CHAR(50)
-        CONSTRAINT column_type_check
-            CHECK (column_type = ANY (ARRAY['characteristic'::bpchar, 'factor'::bpchar])),
-    column_order INTEGER
+    marray VARCHAR(255),
+    sample VARCHAR(255) NOT NULL,
+    annot_value VARCHAR(255) NOT NULL,
+    annot_ont_URI VARCHAR(255),
+    exp_design_column_id INTEGER REFERENCES exp_design_column(id) ON DELETE CASCADE,
+    UNIQUE (sample, exp_design_column_id)
 );
--- flyway/scxa/migrations/V18__scxa-create-exp-design.sql
+++ flyway/scxa/migrations/V18__scxa-create-exp-design.sql
@@ -1,12 +1,3 @@
-CREATE TABLE exp_design
-(
-    sample VARCHAR(255) NOT NULL,
-    annot_value VARCHAR(255) NOT NULL,
-    annot_ont_URI VARCHAR(255),
-    exp_design_column_id INTEGER REFERENCES exp_design_column(id) ON DELETE CASCADE,
-    UNIQUE (sample, exp_design_column_id)
-);
-
 CREATE TABLE exp_design_column
 (
     experiment_accession VARCHAR(255) NOT NULL,
@@ -17,4 +8,13 @@ CREATE TABLE exp_design_column
         CONSTRAINT exp_design_column_sequence_pk
             PRIMARY KEY,
     UNIQUE (experiment_accession, column_name, is_factor)
+);
+
+CREATE TABLE exp_design
+(
+    sample VARCHAR(255) NOT NULL,
+    annot_value VARCHAR(255) NOT NULL,
+    annot_ont_URI VARCHAR(255),
+    exp_design_column_id INTEGER REFERENCES exp_design_column(id) ON DELETE CASCADE,
+    UNIQUE (sample, exp_design_column_id)
 );
