cc6ffb546fc8c440c91207afc522e0b2d9dcd237
Haider Iqbal <haideri@ebi.ac.uk>
2022-03-25 15:09:49 +0000

    Move array_design to exp_design table

--- flyway/gxa/migrations/V8__gxa-create-exp-design.sql
+++ flyway/gxa/migrations/V8__gxa-create-exp-design.sql
@@ -1,7 +1,6 @@
 CREATE TABLE exp_design_column
 (
     experiment_accession VARCHAR(255) NOT NULL,
-    marray VARCHAR(255),
     column_name VARCHAR(255) NOT NULL,
     is_factor BOOLEAN NOT NULL,
     column_order INTEGER NOT NULL,
@@ -14,6 +13,7 @@ CREATE TABLE exp_design_column
 CREATE TABLE exp_design
 (
     sample VARCHAR(255) NOT NULL,
+    array_design VARCHAR(255),
     annot_value VARCHAR(255) NOT NULL,
     annot_ont_URI VARCHAR(255),
     exp_design_column_id INTEGER REFERENCES exp_design_column(id) ON DELETE CASCADE,
