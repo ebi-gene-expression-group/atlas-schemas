4317afed09a8878c6c5f83b714d1b6a7a026d855
Anil Thanki <thanki.anil@gmail.com>
2025-06-25 14:39:14 +0100

    Update V9__gxa-create-exp-design.sql

--- flyway/gxa/migrations/V9__gxa-create-exp-design.sql
+++ flyway/gxa/migrations/V9__gxa-create-exp-design.sql
@@ -2,6 +2,7 @@ CREATE TABLE gxa_marker_gene
 (
     experiment_accession VARCHAR(255) CHECK (experiment_accession LIKE 'E-%') NOT NULL,
     assay VARCHAR(255) NOT NULL,
+    gene_id VARCHAR(255) NOT NULL,
     gene_rank INTEGER NOT NULL,
     expression_value FLOAT NOT NULL,
     expression_unit VARCHAR(8) NOT NULL
