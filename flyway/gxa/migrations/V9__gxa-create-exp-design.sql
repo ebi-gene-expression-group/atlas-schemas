430b56977d1302e24bdd49ac489bec84954d7c23
Anil Thanki <thanki.anil@gmail.com>
2025-06-25 14:36:50 +0100

    Add GXA marker gene table

---
+++ flyway/gxa/migrations/V9__gxa-create-exp-design.sql
@@ -0,0 +1,8 @@
+CREATE TABLE gxa_marker_gene
+(
+    experiment_accession VARCHAR(255) CHECK (experiment_accession LIKE 'E-%') NOT NULL,
+    assay VARCHAR(255) NOT NULL,
+    gene_rank INTEGER NOT NULL,
+    expression_value FLOAT NOT NULL,
+    expression_unit VARCHAR(8) NOT NULL
+);
