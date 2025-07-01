c5e1d5823ea59b0180cc605c8de4180be1bbbc00
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2025-07-01 15:41:02 +0100

    Update V9__gxa-create-exp-design.sql

--- flyway/gxa/migrations/V9__gxa-create-exp-design.sql
+++ flyway/gxa/migrations/V9__gxa-create-exp-design.sql
@@ -3,9 +3,10 @@ CREATE TABLE gxa_marker_gene
     experiment_accession VARCHAR(255) CHECK (experiment_accession LIKE 'E-%') NOT NULL,
     assay VARCHAR(255) NOT NULL,
     gene_id VARCHAR(255) NOT NULL,
+    gene_name VARCHAR(255) NOT NULL,
     gene_rank INTEGER NOT NULL,
     expression_value FLOAT NOT NULL,
     expression_unit VARCHAR(8) NOT NULL,
-    gene_name VARCHAR(255) NOT NULL,
+    sample_number INTEGER NOT NULL,
     CONSTRAINT gxa_marker_gene_pkey PRIMARY KEY (experiment_accession, assay, gene_id, gene_rank)
 );
