75124edee090abc0ca16c2c3757882c41a7aa4e3
Anil Thanki <thanki.anil@gmail.com>
2025-07-03 10:49:33 +0100

    Update V9__gxa-create-exp-design.sql - removes `assay_id` and `number_assays` and rearranges cols to support file

--- flyway/gxa/migrations/V9__gxa-create-exp-design.sql
+++ flyway/gxa/migrations/V9__gxa-create-exp-design.sql
@@ -2,13 +2,11 @@ CREATE TABLE gxa_marker_gene
 (
     experiment_accession VARCHAR(255) CHECK (experiment_accession LIKE 'E-%') NOT NULL,
     assay VARCHAR(255) NOT NULL,
-    assay_id VARCHAR(255) NOT NULL,
     gene_id VARCHAR(255) NOT NULL,
-    gene_name VARCHAR(255) NOT NULL,
-    marker_gene_rank INTEGER,
-    expression_level FLOAT NOT NULL,
-    expression_unit VARCHAR(8) NOT NULL,
-    number_assays INTEGER NOT NULL,
     specificity_score FLOAT,
+    marker_gene_rank INTEGER,
+    expression_unit VARCHAR(8) NOT NULL,
+    expression_level FLOAT NOT NULL,
+    gene_name VARCHAR(255) NOT NULL,
     CONSTRAINT gxa_marker_gene_pkey PRIMARY KEY (experiment_accession, assay, gene_id)
 );
